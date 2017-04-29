d1b6d1acdd7e112bf2bdb1a62b037f9bfbcd768d
Swall0w <technext.jpn@gmail.com>
2017-04-29 23:11:44 +0900

    it doesnt work

--- train.py
+++ train.py
@@ -16,6 +16,29 @@ def arg():
     parser.add_argument('--unit','-u',type=int,default=500,help='number of unit')
     return parser.parse_args()
 
+def out_gen_image(generator, H, W, rows, cols, dst):
+    @chainer.training.make_extension()
+    def make_image(trainer):
+        n_images = rows * cols
+        xp = generator.xp
+        z = xp.random.randn(n_images, 1, H/4, W/4).astype(xp.float32)
+        x = generator(z, test = True)
+        x = chainer.cuda.to_cpu(x.data)
+
+        x = np.array(np.clip(x * 255, 0.0, 255.0),dtype=np.unit8)
+        channels = x.shape[1]
+        x = x.reshape((rows, cols, channels, H, W))
+        x = x.transpose(0,3,1,4,2)
+        x = x.reshape((rows * H, cols * W, channels))
+        x = np.squeeze(x)
+
+        preview_dir = '{}/preview'.format(dst)
+        preview_path = preview_dir + '/image{:0>5}.png'.format(trainer.updater.epoch)
+        if not os.path.exits(preview_dir):
+            os.makedirs(preview_dir)
+        Image.fromarray(x).save(preview_path)
+    return make_image
+
 def main():
     args = arg()
     generator = Generator()
@@ -41,6 +64,18 @@ def main():
     updater = WGANUpdater(train_iter, generator, critic, 5, op_g, op_c, device=args.gpu)
     trainer = training.Trainer(updater, (args.epoch,'epoch'),out=args.output)
 
+    trainer.extend(extensions.dump_graph('wasserstein distance'))
+    trainer.extend(extensions.snapshot(), trigger=(args.epoch,'epoch'))
+    trainer.extend(extensions.LogReport())
+    trainer.extend(extensions.PlotReport(['wasserstein distance'],'epoch', file_name = 'distance.png'))
+    trainer.extend(extensions.PlotReport(['epoch','wasserstein distance','loss/generator','elapsed_time']))
+    trainer.extend(extensions.ProgressBar())
+    trainer.extend(out_gen_image(generator,28,28,5,5,args.output),trigger=(1,'epoch'))
+
+    if args.resume:
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    trainer.run()
 
 if __name__ == '__main__':
     main()
